515f5fd983466a9bd9c97d75164b357acbde7b21
Tony <284096905@qq.com>
2017-01-17 11:01:22 +0800

    Initial Commit

---
+++ TLRuntimeTest/TLRuntimeTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TLRuntimeTest
+//
+//  Created by Tony on 2017/1/17.
+//  Copyright © 2017年 Tony. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
